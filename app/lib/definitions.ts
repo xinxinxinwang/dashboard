43706aa045f7f2af82dfde5f0002dcbe7c9a8529
wangxin <347720427@qq.com>
2025-12-12 11:06:17 +0800

    chapter11 finished

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -25,7 +25,6 @@ export type Invoice = {
   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
   status: 'pending' | 'paid';
 };
-
 export type Revenue = {
   month: string;
   revenue: number;
@@ -86,3 +85,4 @@ export type InvoiceForm = {
   amount: number;
   status: 'pending' | 'paid';
 };
+
